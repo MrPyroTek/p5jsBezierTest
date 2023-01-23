849586ccc3019519b173496d0ba1a501aedd5036
Tek <41598027+MrPyroTek@users.noreply.github.com>
2023-01-23 10:19:33 +0100

    Add gradient and fix small issues

--- sketch.js
+++ sketch.js
@@ -31,9 +31,12 @@ function generate(){
         createVector(rdm(i), rdm(j)),
         [rdmc(i,j), rdmc(i,j), rdmc(i,j)],
         [rdmc(i,j), rdmc(i,j), rdmc(i,j)],
+        [100,100,100]
       ]
+      matrice[i][j][8] = [matrice[i][j][6][0],matrice[i][j][6][1],matrice[i][j][6][2]]
     }
   }
+  
 }
 
 function regen(){
@@ -50,6 +53,8 @@ function regen(){
         createVector(rdm(i), rdm(j)),
         matrice[i][j][6],
         [rdmc(i,j), rdmc(i,j), rdmc(i,j)],
+        [100,100,100]
+        
       ]
     }
   }
@@ -69,6 +74,7 @@ function regenrdm(){
         createVector(rdm(i), rdm(j)),
         matrice[i][j][6],
         [rdmc(i,j), rdmc(i,j), rdmc(i,j)],
+        matrice[i][j][8]
       ]
 }
 
@@ -83,6 +89,49 @@ function draw() {
   for (let i = 0; i < nombrecarrer; i++) {
     for (let j = 0; j < nombrecarrer; j++) {
 
+
+
+
+
+      //dessine le gradient en dessous de la courbe
+
+      beginShape();
+      noStroke();
+      if(j+1<nombrecarrer){
+      fillGradient('linear', {
+        from : [i*tc+(tc/2),j*tc+(tc/2)],   // x, y : Coordinates
+        to : [i*tc+(tc/2),j*tc+tc], // x, y : Coordinates
+        steps : [
+            color(matrice[i][j][6][0],matrice[i][j][6][1],matrice[i][j][6][2]),
+            color(matrice[i][j+1][8][0],matrice[i][j+1][8][1],matrice[i][j+1][8][2])
+            //color(255)
+          ] // Array of p5.color objects or arrays containing [p5.color Object, Color Stop (0 to 1)]
+    });} else {
+      fill(100)
+    }
+
+
+      vertex(matrice[i][j][0].x+2,matrice[i][j][0].y,);
+      if(i+1>=nombrecarrer) {
+        bezierVertex(
+          matrice[i][j][1].x, matrice[i][j][1].y,
+          matrice[i][j][2].x, matrice[i][j][2].y,
+          matrice[i][j][0].x+tc,matrice[0][j][0].y);
+          vertex(matrice[i][j][0].x+tc+1,matrice[0][j][0].y);
+          vertex(i*tc+tc,j*tc+tc)
+          vertex(i*tc,j*tc+tc);
+      } else {
+        bezierVertex(
+          matrice[i][j][1].x, matrice[i][j][1].y,
+          matrice[i][j][2].x, matrice[i][j][2].y,
+          matrice[i+1][j][0].x+1,matrice[i+1][j][0].y);
+          vertex(i*tc+tc,j*tc+tc);
+          vertex(i*tc,j*tc+tc);
+      }
+      endShape();
+
+
+      //dessine la courbe
       noFill();
       strokeWeight(5);
       stroke(matrice[i][j][6][0]+random(-5,5),matrice[i][j][6][1]+random(-5,5),matrice[i][j][6][2]+random(-5,5));
@@ -91,12 +140,25 @@ function draw() {
           matrice[i][j][1].x, matrice[i][j][1].y,
           matrice[i][j][2].x, matrice[i][j][2].y,
           matrice[i][j][0].x+tc-2,matrice[0][j][0].y);
+          vertex(matrice[i][j][0].x+tc+1,matrice[0][j][0].y);
+          vertex(i*tc+tc,j*tc+tc)
+          vertex(i*tc,j*tc+tc);
       } else {
         bezier(matrice[i][j][0].x+2,matrice[i][j][0].y,
           matrice[i][j][1].x, matrice[i][j][1].y,
           matrice[i][j][2].x, matrice[i][j][2].y,
-          matrice[i+1][j][0].x-2,matrice[i+1][j][0].y);
+          matrice[i+1][j][0].x+1,matrice[i+1][j][0].y);
+          vertex(i*tc+tc,j*tc+tc);
+          vertex(i*tc,j*tc+tc);
       }
+
+      fill(255)
+
+
+
+
+
+
 
       matrice[i][j][0].x = lerp(matrice[i][j][0].x,matrice[i][j][3].x,force);
       matrice[i][j][0].y = lerp(matrice[i][j][0].y,matrice[i][j][3].y,force);
@@ -119,7 +181,7 @@ function drawlines() {
     for (let j = 0; j < nombrecarrer; j++) {
       noStroke();
       fill(matrice[i][j][6][0],matrice[i][j][6][1],matrice[i][j][6][2]);
-      square(i*tc, j*tc, tc);
+      square(i*tc, j*tc, tc+2);
       //fill(255,0,0);
       //ellipse(matrice[i][j][0].x,matrice[i][j][0].y, 10,10);
 
